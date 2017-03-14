844353191a77e02963df6ff3698c5edeb4d7cdc6
SamyHamiche <hamiche.samy@gmail.com>
2017-03-14 11:37:48 +0100

    .gitignore+test commit

--- LeBonDZ/src/main/java/org/lebondz/model/Commentaire.java
+++ LeBonDZ/src/main/java/org/lebondz/model/Commentaire.java
@@ -7,7 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-
+/* Samy test */
 @Entity
 public class Commentaire {
 
